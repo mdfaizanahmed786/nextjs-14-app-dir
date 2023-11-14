43ccc6c9b078e29844cb04b5b6dc05bcc970ffe8
Faizan Ahmed <faizanzaid789@gmail.com>
2023-11-14 19:27:11 +0530

    feat: added dynamc segment pages

---
+++ app/docs/[...slug]/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// /docs/feature/concept this catch-all segment is represented by the slug array
+// here feature and concept are dynamic segments
+function DocsPage({
+  params,
+}: {
+  params: {
+    slug: string[];
+  };
+}) {
+  if (params.slug[0] === "feature" && params.slug[1] === "concept") {
+    return <div>Feature Concept</div>;
+  } else if (params.slug[0] === "feature" && params.slug[1] === "api") {
+    return <div>Feature API</div>;
+  }
+
+  return <div>DocsPage</div>;
+}
+
+export default DocsPage;
+
+// when we route to /docs page it throws 404 page, to avoid that we need to add [[...slug]]
