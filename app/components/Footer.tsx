599f906edd2a4a84680deee10df61e6ec1b3aadb
Faizan Ahmed <faizanzaid789@gmail.com>
2023-11-15 16:17:59 +0530

    feat: understood layout more in depth

---
+++ app/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="bg-green-500 w-full sticky bottom-0 h-10">Footer</div>;
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div className="bg-purple-500 w-full h-10">Header</div>;
+}   
