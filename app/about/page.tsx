5d39f04d1e67daa6b063265858a4b5264aa82627
Faizan Ahmed <faizanzaid789@gmail.com>
2023-11-20 14:41:24 +0530

    feat: added generate meta data function

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "About Page",
+};
 export default function About() {
   return <div>About</div>;
 }
--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { Metadata } from "next";
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+// below is the generate  metadata function
+export async function generateMetadata({ params }: Props) {
+  // some async calls can also be made here
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone Product ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title,
+    description: `Product ${params.productId} description`,
+  };
+}
 
-function ProductId({ params }: { params: { productId: string } }) {
+function ProductId({ params }: Props) {
   return <div>You are viewing product with id: {params.productId}</div>;
 }
 
