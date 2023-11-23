7a72e9349153121bf140ff4facbfc206b5cece54
Faizan Ahmed <faizanzaid789@gmail.com>
2023-11-23 17:46:02 +0530

    feat: demo of templates

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function AuthLayout({
   children,
@@ -23,9 +24,16 @@ export default function AuthLayout({
     },
   ];
 
+  const [value, setValue] = useState("");
   return (
     <div>
       <h1>Auth Layout</h1>
+      {/* Below the input is used to demonstrate the state preserve between different pages ONLY IN LAYOUT.TSX not IN TEMPLATE.TSX */}
+      <input
+        value={value}
+        className="text-green-500"
+        onChange={(e) => setValue(e.target.value)}
+      />
       {navLinks.map((link) => (
         <Link href={link.href} key={link.label}>
           {pathname.startsWith(link.href) ? (
@@ -39,3 +47,7 @@ export default function AuthLayout({
     </div>
   );
 }
+
+// Important: In layout.tsx file, the state remains unaffected when navigating to other pages, because layout will only observe which are not common, the common ones will not get affected. But what if you are wishing to perform some sideffects or some animations different on the pages?? so to solve this we can use template.tsx or tempalte.js file. but we need to avoid this file as much as possible, because it will be rendered on every page.
+
+// Templates: They are also much similar to layout.tsx file and they accept children as props.
