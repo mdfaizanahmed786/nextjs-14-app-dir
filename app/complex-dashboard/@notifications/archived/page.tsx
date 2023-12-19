d9f989d6c98d50a5b0cb9cb8e650169ebde10294
Faizan Ahmed <faizanzaid789@gmail.com>
2023-12-19 16:32:33 +0530

    folder: added default .tsx files for routes

---
+++ app/complex-dashboard/@notifications/archived/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function ArchivedNotifcations(){
+    return <div>ArchivedNotifcations
+
+        <Link className="text-blue-500" href="/complex-dashboard">
+          Default
+        </Link>
+    </div>
+}
---
+++ app/complex-dashboard/default.tsx
@@ -0,0 +1,5 @@
+
+// This will serve as fallback view for children slot
+export default function DefaultComplexDashboard() {
+  return <div>DefaultComplexDashboard</div>;
+}
