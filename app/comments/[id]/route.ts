ee2931a551b0b3380b3aafcf6e7e201d9bf9acff
Faizan Ahmed <faizanzaid789@gmail.com>
2024-01-12 02:39:51 +0530

    added rute handlers

---
+++ app/comments/[id]/route.ts
@@ -0,0 +1,54 @@
+import { comments } from "../data";
+
+export async function GET(
+  _request: Request,
+  {
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const comment = comments.find((comment) => comment.id === Number(params.id));
+
+  return Response.json(comment);
+}
+
+export async function PATCH(
+  request: Request,
+  {
+    params,
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  const indx = comments.findIndex(
+    (comment) => comment.id === Number(params.id)
+  );
+  if (indx == -1) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const data = await request.json();
+
+  comments[indx] = {
+    id: Number(params.id),
+    text: data.text,
+  }
+  return new Response("Updated", { status: 201 });
+}
+
+
+export async function DELETE(_request: Request, { params }: { params: { id: string } }) {
+  const indx = comments.findIndex((comment) => comment.id === Number(params.id));
+  if (indx == -1) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  comments.splice(indx, 1);
+  return new Response("Deleted", { status: 201 });
+
+}
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
-  publicRoutes: ["/", "/comments"]
+  publicRoutes: ["/", "/comments/[id]"]
 });
  
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)"],
 };
